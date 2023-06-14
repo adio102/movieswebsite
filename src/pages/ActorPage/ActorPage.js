2705bbf10d8086b022f4fd552e3d54bf76ab4179
unknown <adrian.adamus99@gmail.com>
2023-06-14 18:31:28 +0200

    second commit

---
+++ src/pages/ActorPage/ActorPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './ActorPage.module.css'
+
+const Actor = () => {
+  return (
+    <div>
+      <div>Actor</div>
+    </div>
+  )
+}
+
+export default Actor
---
+++ src/pages/MoviePage/MoviePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './MoviePage.module.css'
+
+const Movie = () => {
+  return (
+    <div>
+      <div>MoviePage</div>
+    </div>
+  )
+}
+
+export default Movie
